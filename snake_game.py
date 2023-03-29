10e012c0ff68ece680e3daf521caf686e6964bb6
G Sravan Kumar <kumarsravangs@gmail.com>
2023-03-29 21:07:16 +0530

    Init commit of snake game

---
+++ snake_game.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+
+
+class SnakeGame:
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+
+        # init display
+
+        # init game state
+
+    def play_step(self):
+        pass
+
+
+
+if __main__ == "__main__":
+    game = SnakeGame()
+
+    # game loop
+    while True:
+        game.play_step()
+
+        # break if game is over
+
+    pygame.quit()
